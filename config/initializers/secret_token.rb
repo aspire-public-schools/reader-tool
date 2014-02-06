8aa422baa0b896ba6f61c78d2d63ca0e7fd7686a
gary1410 <gmingtsai@gmail.com>
2014-02-06 11:15:54 -0800

    added standard rails files

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ReaderTool::Application.config.secret_token = '2826cd2b732b8036a923e81b143d7a2bdd2e73c1bc640938b13744af32d5f85580ddfd3e3bef69a20971c1de0aca9c7aed3adaf4311cf4fb50a68de92be6a659'
