196de774df8e16be2af18fc071957d91dd9109a3
gary1410 <gmingtsai@gmail.com>
2014-02-15 14:57:53 -0800

    added bootstrap for rails and navbar fix

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,11 +6,11 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-
 reader1 = Reader.create(email: "benjamin.crosby@aspirepublicschools.org", employee_number: 2043, first_name: "Benjamin", last_name: "Crosby")
 reader2 = Reader.create(email: "matt.seigel@aspirepublicschools.org", employee_number: 1491, first_name: "Matthew", last_name: "Seigel")
 reader3 = Reader.create(email: "kimberly.whitehead@aspirepublicschools.org", employee_number: 2862, first_name: "Kimberly", last_name: "Whitehead")
 reader4 = Reader.create(email: "james.gallagher@aspirepublichschools.org", employee_number: 1118, first_name: "James", last_name: "Gallagher")
 reader5 = Reader.create(email: "abbey.esposto@aspirepublicschools.org", employee_number: 5058, first_name: "Abbey", last_name: "Esposto")
 reader6 = Reader.create(email: "julie.tsai@aspirepublicschools.org", employee_number: 3605, first_name: "Julie", last_name: "Tsai")
+
 
