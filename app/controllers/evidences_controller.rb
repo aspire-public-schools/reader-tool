e05621b87a51670fe68f5fadcd115849acbc38b6
gary1410 <gmingtsai@gmail.com>
2014-03-05 11:21:08 -0800

    removed def update in evidences controller and added partial in observations index

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -7,15 +7,6 @@ class EvidencesController < ApplicationController
     # @evidence_score = @indicator.evidence_scores
   end
 
-  def update
-      if @evidence_scores.update_attributes(params[:evidence_score.id])
-        flash[:success] = "Completed"
-        redirect_to
-      else
-        redirect_to
-      end
-  end
-
   def score
       params[:evidence_scores].keys.each do |score_id|
         quality = params[:evidence_scores][score_id][:quality] || false
