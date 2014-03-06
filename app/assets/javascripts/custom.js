e3a61f53d8a2f9b123f8be33d743941c09cd8cd0
gary1410 <gmingtsai@gmail.com>
2014-03-05 16:25:47 -0800

    evidence form shows up on observations index via ajax

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,9 +1,25 @@
-$(document).ready(function(){
-
+var sideBar = {
+  toggle: function () {
     $('dl').on('click', 'dt', function(){
       var $this = $(this),
-          $firstGroup = $this.nextUntil('dt');
+        $firstGroup = $this.nextUntil('dt');
       $firstGroup.toggle('fast', 'swing');
       $this.siblings('dd').not($firstGroup).hide()
     })
+  }
+}
+
+var evidenceScore = {
+    init: function () {
+      $(document).on("ajax:success", this.appendEvidence)
+    },
+    appendEvidence: function(event, data, status, xhr) {
+        $('form').remove()
+        $('.evidence-form').append(data.evidence_list)
+    }
+  }
+
+$(document).ready(function(){
+  sideBar.toggle();
+  evidenceScore.init();
 })
--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -1,10 +1,16 @@
 class EvidencesController < ApplicationController
 
   def index
+    # @domains = @observation_read.domain_scores
     # observation = ObservationRead.find(params[:observation_id])
     # domain = DomainScore.find(params[:domain_id])
     @indicator_score = IndicatorScore.find(params[:indicator_id])
     # @evidence_score = @indicator.evidence_scores
+    if @indicator_score
+      render :json => { :evidence_list => render_to_string( :partial => "evidence_score_form", locals: { :indicator_score => @indicator_score} ) }
+    else
+      render :json => { :status => :unprocessable_entity }
+    end
   end
 
   def score
