4be5fcbea8cf86b1cb9144a4436eabc7e186014e
gary1410 <gmingtsai@gmail.com>
2014-03-07 14:23:05 -0800

    added table dashboard

--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -37,12 +37,8 @@ $(document).ready(function(){
   // submitScore.init();
 
   $('#myTab a').click(function (e) {
-  e.preventDefault()
-  $(this).tab('show')
-})
+    e.preventDefault()
+    $(this).tab('show')
+  })
 
-$('#myTab a[href="#profile"]').tab('show') // Select tab by name
-$('#myTab a:first').tab('show') // Select first tab
-$('#myTab a:last').tab('show') // Select last tab
-$('#myTab li:eq(2) a').tab('show') // Select third tab (0-indexed)
 })
