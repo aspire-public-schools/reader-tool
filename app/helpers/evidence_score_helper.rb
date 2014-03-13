b637054538fd3667d39b2cc660fe489f9da1c478
gary1410 <gmingtsai@gmail.com>
2014-03-12 18:10:58 -0700

    percentages show up on the dashboard

--- app/helpers/evidence_score_helper.rb
+++ app/helpers/evidence_score_helper.rb
@@ -1,24 +1,24 @@
  module EvidenceScoreHelper
 
-  def get_evidence_scores(obs_read_id, ind_code)
-     sql = "SELECT evds.*, inds.*
-      FROM evidence_scores evds
-      LEFT JOIN indicator_scores inds
-           ON evds.indicator_score_id = inds.id
-      LEFT JOIN domain_scores doms
-           ON inds.domain_score_id = doms.id
-      LEFT JOIN observation_reads obsr
-           ON doms.observation_read_id = obsr.id
-      LEFT JOIN indicators ind
-      ON inds.indicator_id = ind.id
+  def get_percentages(observation_id)
 
-      WHERE
-      observation_read_id = '#{obs_read_id}''
-      AND ind.code = '#{ind_code}'
-      ORDER BY obsr.id, ind.code"
+    sql = "SELECT obr.id, dom.number,AVG(evds.quality::integer) AS quality_average,  AVG(evds.alignment::integer) AS alignment_average
+FROM observation_reads obr
+LEFT JOIN domain_scores doms
+                ON doms.observation_read_id = obr.id
+LEFT JOIN domains dom
+                ON doms.domain_id = dom.id
+LEFT JOIN indicator_scores inds
+                ON inds.domain_score_id = doms.id
+LEFT JOIN evidence_scores evds
+                ON evds.indicator_score_id = inds.id
+WHERE obr.id = #{observation_id}
+GROUP BY obr.id, dom.number"
 
-      @evidence_scores = EvidenceScore.find_by_sql(sql)
-  end
+  @observation_scores = ObservationRead.find_by_sql(sql)
+
+end
+
 end
 
 
