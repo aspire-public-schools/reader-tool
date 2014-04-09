6b941c7ddadab41457a8cde8c0d7791df6a3f970
gary1410 <gmingtsai@gmail.com>
2014-04-08 22:10:57 -0700

    redid login forms to center on page

--- app/controllers/evidences_controller.rb
+++ app/controllers/evidences_controller.rb
@@ -13,14 +13,16 @@ class EvidencesController < ApplicationController
   def score
       @reader = current_user
       @observation_read = @reader.observation_reads.find(params[:observation_id])
-      params[:evidence_scores].keys.each do |score_id|
-        quality = params[:evidence_scores][score_id][:quality] || false
-        alignment = params[:evidence_scores][score_id][:alignment] || false
-        @evidence_score_update = EvidenceScore.update(score_id,{quality: quality, alignment: alignment})
+      if params[:evidence_scores] != nil
+        params[:evidence_scores].keys.each do |score_id|
+          quality = params[:evidence_scores][score_id][:quality] || false
+          alignment = params[:evidence_scores][score_id][:alignment] || false
+          @evidence_score_update = EvidenceScore.update(score_id,{quality: quality, alignment: alignment})
+        end
       end
         @indicator_score_update = IndicatorScore.update(params[:indicator_id], {comments: params[:comments] })
+      if @indicator_score_update
         @indicator_score = IndicatorScore.find(params[:indicator_id])
-      if @evidence_score_update
         @get_section_scores = get_section_scores(params[:observation_id])
         @domain_percentages = get_percentages(params[:observation_id])
         @domain_percentages_sort = @domain_percentages.sort! { |a,b| a.number <=> b.number }
@@ -30,7 +32,7 @@ class EvidencesController < ApplicationController
                             :domain_percentages => render_to_string(:partial => 'observations/domain_percentages'),
                                             locals: { :domain_percentages_sort => @domain_percentages_sort, :get_section_scores => @get_section_scores, :observation_read => @observation_read} }
         else
-          render :json => { :error => reader.errors.full_messages.join(", ")}, :status => :unprocessable_entity
+          render :json => { :error => @reader.errors.full_messages.join(", ")}, :status => :unprocessable_entity
       end
   end
 end
