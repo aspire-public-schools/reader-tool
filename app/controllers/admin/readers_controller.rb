fcf98396dcbf3195d378a98f552e8020545ea704
gary1410 <gmingtsai@gmail.com>
2014-05-27 14:21:47 -0700

    adds ability to create new readers in the admin page

--- app/controllers/admin/readers_controller.rb
+++ app/controllers/admin/readers_controller.rb
@@ -1,10 +1,28 @@
 class Admin::ReadersController < ApplicationController
 
+  def create
+    reader = Reader.new(params[:reader])
+    if reader.save
+      redirect_to admin_readers_path, :flash => { :success => "Your changes were saved!" }
+    else
+      render new_admin_reader_path, :flash => { :error => "Check your fields"}
+    end
+  end
+
   def new
+    @reader = Reader.new
   end
 
   def index
+    @readers = Reader.all
   end
 
+  def edit
+    @readers = Reader.all
+  end
+
+  def show
+    @readers = Reader.all
+  end
 
 end
