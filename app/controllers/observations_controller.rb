ee3583eb749f6b99b55c54e88ee199b66e4be2e2
gary1410 <gmingtsai@gmail.com>
2014-06-04 11:35:51 -0700

    add before filter for reader observations and SQL code for reader flags

--- app/controllers/observations_controller.rb
+++ app/controllers/observations_controller.rb
@@ -1,32 +1,13 @@
  class ObservationsController < ApplicationController
   include EvidenceScoreHelper
   before_filter :require_reader
+  before_filter :reader_observations, :only => [:index, :show]
 
 	def index
-    @reader = current_user
-    @observation_reads = @reader.observation_reads
-    @observation_reads = @reader.observation_reads.where(observation_status: 2)
-    @domains = Domain.all
-    @domain = Domain.all
-    if params[:domain_id]
-        @indicator = Domain.find(params[:domain_id]).indicators
-      if params[:indicator_id]
-        @evidence_scores = Indicator.find(params[:indicator_id]).evidence_scores
-      end
-    end
-    render 'index'
   end
 
   def show
-    @reader = current_user
-    @observation_reads = ObservationRead.where(observation_status: 2)
     @observation_read = ObservationRead.find(params[:id])
-    if params[:domain_id]
-      @indicator = Domain.find(params[:domain_id]).indicators
-      if params[:indicator_id]
-        @evidence_scores = Indicator.find(params[:indicator_id]).evidence_scores
-      end
-    end
     @domains = @observation_read.domain_scores
     @domain_percentages = get_percentages(params[:id])
     @get_section_scores = get_section_scores(params[:id])
@@ -53,6 +34,11 @@
 
   def require_reader
     redirect_to "/login" unless current_user
+  end
+
+  def reader_observations
+    @reader = current_user
+    @observation_reads = @reader.observation_reads.where(observation_status: 2)
   end
 
 end
