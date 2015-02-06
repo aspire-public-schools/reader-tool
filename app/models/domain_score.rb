b685b7b8b6f6c9cbf104f472e54dfbe7cfb7637e
David Lowenfels <david@internautdesign.com>
2015-02-05 16:40:04 -0800

    fix has_many

--- app/models/domain_score.rb
+++ app/models/domain_score.rb
@@ -1,7 +1,7 @@
 class DomainScore < ActiveRecord::Base
   has_many :indicator_scores
   has_many :indicators, through: :indicator_scores
-  has_many :evidences, through: :indicator_scores
+  has_many :evidence_scores, through: :indicator_scores
   belongs_to :observation_read
   belongs_to :domain
 end
