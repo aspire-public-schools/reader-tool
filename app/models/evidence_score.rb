9b10ce45532e5d77f7acd64a0105aedfe16cb320
David Lowenfels <david@internautdesign.com>
2015-02-24 10:11:01 -0800

    more formatting correction for evidence_score

--- app/models/evidence_score.rb
+++ app/models/evidence_score.rb
@@ -3,4 +3,8 @@ class EvidenceScore < ActiveRecord::Base
   belongs_to :indicator_score
 
   delegate :domain_score, :domain, :to => :indicator_score
+
+  def formatted_description
+    description.gsub(/\s([TS](\(\w+\))?:?[\s\/])/){  "\n"+$1 }.gsub("\n","<br/>"*2) 
+  end
 end
