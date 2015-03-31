904846d3fb1f1597ca5701807298a76a65c2bd6b
David Lowenfels <david@internautdesign.com>
2015-03-30 17:40:32 -0700

    cleanup

--- app/helpers/observation_reads_helper.rb
+++ app/helpers/observation_reads_helper.rb
@@ -4,17 +4,6 @@ module ObservationReadsHelper
     @dropdown.find{|d| d.last == value }.try(:first)
   end
 
-  def print_reader_number observation_read
-    case observation_read.reader_number
-    when '1a'
-      '1'
-    when '1b'
-      '2'
-    when '2'
-      '3'
-    end
-  end
-
   def highlight_if_showing obs
     # p "obs: #{obs}  params:#{params}"
     return 'selected' if obs.id == params[:id].to_i
