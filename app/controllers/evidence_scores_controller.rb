371cb3a8f66bfff64d078df0ba30dd0202ca13b4
David Lowenfels <dfl@cosminyah.local>
2015-05-18 22:48:44 -0700

    fix typo comment from commit 3b1239fe

--- app/controllers/evidence_scores_controller.rb
+++ app/controllers/evidence_scores_controller.rb
@@ -3,7 +3,7 @@ class EvidenceScoresController < ApplicationController
   def index
     if @indicator_score = IndicatorScore.find(params[:indicator_score_id]) 
       @observation_read = @indicator_score.observation_read
-      # p @evidence_scores  = @indicator_score.evidence_scores
+      @evidence_scores  = @indicator_score.evidence_scores
       render json: { evidence_list: render_to_string( partial: "evidence_score_form" ) }
     else
       render json: { error: reader.errors.full_messages.join(", ")}, status: :unprocessable_entity
